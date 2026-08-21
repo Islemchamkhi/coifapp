4d829e04aa59cb7befefe45452287993128a9b35
islem <isslemchamkhii@gmail.com>
2026-08-21 14:34:40 +0100

    Fix CSS import path

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import { ClientAuthProvider } from "./auth/ClientAuthContext";
 import { LanguageProvider } from "./i18n/LanguageContext";
+
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <BrowserRouter>
       <LanguageProvider>
